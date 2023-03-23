611ad3de5241762bc567f995bac67ad8dbc63f75
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-03-23 23:59:59 +0530

    Added execute function

--- main.c
+++ main.c
@@ -117,6 +117,19 @@ int shell_exit(char **args) {
 	return 0;
 }
 
+int execute(char **args) {
+	if (args[0] == NULL) {
+		return 1;
+	}
+
+	for (int i = 0; i < num_builtins(); i++) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
+
+	return launch(args);
+}
 
 void loop(void) {
 	char prompt = '$';
